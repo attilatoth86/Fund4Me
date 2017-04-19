eb6fc0f681fa2ab4bc99780d7ac483417d48ff49
attilatoth86 <attila.toth86@gmail.com>
2017-04-19 22:24:43 +0200

    app.R:  - removing shinyjs library /not in use/  - designing plot boxes  - replace menuSubItem() to menuItem() for more customization  - get rid of unused code snippets  - adding warning message on front page  - linking github repo on Technology tab  - creating content for Terms of Use tab  - renaming pages

--- app.R
+++ app.R
@@ -3,7 +3,6 @@
 
 library(shiny) # http://shiny.rstudio.com
 library(shinydashboard) # https://rstudio.github.io/shinydashboard/
-library(shinyjs) # https://github.com/daattali/shinyjs
 library(plotly) # https://plot.ly
 library(DT) # https://rstudio.github.io/DT/
 library(PerformanceAnalytics) # https://www.rdocumentation.org/packages/PerformanceAnalytics/
@@ -93,14 +92,17 @@ server <- function(input, output, session) {
       fluidRow(
         column(width = 6,
                box(title = unique(chrt_dt["fund_name"][chrt_dt$fund_id==i,]),
-                   plotlyOutput(paste0("dyn_plot_out",i)),
                    width = NULL,
-                   solidHeader = T
+                   solidHeader = T,
+                   plotlyOutput(paste0("dyn_plot_out",i))
                 )
                ),
         column(width = 6,
-               box(width = NULL,
-                   dataTableOutput(paste0("dyn_table_out",i)))
+               box(title="Performance, Return & Risk",
+                   status = "primary",
+                   width = NULL,
+                   dataTableOutput(paste0("dyn_table_out",i))),
+                   "Historical performance indications are no guarantee for current or future performance. Performance indications do not consider commissions levied at subscription and/or redemption."
                )
       )
     })
@@ -134,8 +136,8 @@ ui <- dashboardPage(
                    sidebarMenu(
                      menuItem("Home", tabName = "home", icon = icon("home")),
                      menuItem("Funds",tabName = NULL, icon = icon("line-chart"),
-                              menuSubItem("Overview", tabName = "funds_ov"),
-                              menuSubItem("Prices", tabName = "fundprices")
+                              menuItem("Overview", tabName = "funds_ov", icon = icon("angle-right"), badgeLabel = "disabled", badgeColor = "red"),
+                              menuItem("Details", tabName = "fundprices", icon = icon("angle-right"))
                      )
                    ) # sidebarMenu() end
   ),
@@ -143,7 +145,6 @@ ui <- dashboardPage(
 # UI dashboardBody() start ------------------------------------------------
 
   dashboardBody(
-    #tags$head(tags$style(HTML('.info-box {min-height: 45px;} .info-box-icon {height: 45px; line-height: 45px;} .info-box-content {padding-top: 0px; padding-bottom: 0px;}'))),
 
 # UI tabItems() start -----------------------------------------------------
 
@@ -177,7 +178,14 @@ ui <- dashboardPage(
                                               tags$li("exploring difficulties around setup and ownership of a",tags$b("cloud"),"based machine,"),
                                               tags$li("creating and managing my ",tags$b("own analytical stack,")),
                                               tags$li(tags$b("demonstrating skills"),"and techniques obtained during my journey with R ecosystem.")
-                                              )
+                                              ),
+                                       br(),
+                                       div(class="alert alert-warning", #"alert alert-danger",
+                                           role="alert",
+                                           h4(icon("exclamation-triangle"), " Warning"),
+                                           tags$b("Every investment involves risk. It is highly recommended to consult a professional advisor about your personal circumstances before making investment decision."),
+                                           tags$b("Historical performance indications and financial market scenarios are no guarantee for current or future performance.")
+                                        )
                                        ), # tabPanel() end
                               tabPanel("The App",
                                        h4("Purpose"),
@@ -218,7 +226,7 @@ ui <- dashboardPage(
                                               "Efforts invested into this application intended to create a so-called data project by exploring and exploiting opportunities provided by",tags$b("R ecosystem."),
                                               "However, in order to bring this product into life multiple components of various technologies must have been coordinated and harmonized.",
                                               "Given the characteristics and the purposes of the project, the implemented solution depends solely on",tags$b("open source"),"(free) tools and components.",
-                                              "In order to promote the open source movement further and give back something to the community, the entire source code is available on this public repository.",
+                                              "In order to promote the open source movement further and give back something to the community, the entire source code is available on",a("this",href="https://github.com/attilatoth86/Fund4Me",target="_blank"),"public repository.",
                                               br(),br(),
                                               h4("Highlights"),
                                               tags$ul(
@@ -229,18 +237,30 @@ ui <- dashboardPage(
                                                 tags$li(a("GitHub",href="https://github.com/",target="_blank")," does the version control and source code management for the project.")
                                               )
                                            ), # tabPanel() end 
-                              tabPanel("Disclaimer", "Disclaimer content")
+                              tabPanel("Terms of Use",
+                                       "By accessing this web site, the pages contained on it, the products, services, information, tools and material contained or described herein (the \"Site\"), you acknowledge your agreement with and understanding of the following terms of use.",
+                                       br(),br(),
+                                       h4("Information supplied"),
+                                       "The content on this website was produced by Attila Toth (hereafter \"Author\") with the greatest of care and to the best of his knowledge and belief. However, Author provides no guarantee with regard to its content and completeness and does not accept any liability for losses which might arise from making use of the information contained herein. All details are provided for information purposes only. The information provided herein is not legally binding and it does not constitute an offer or invitation to enter into any type of financial transaction. The recipient is in particular recommended to check that the information provided is in line with his/her own circumstances with regard to any legal, regulatory, tax or other consequences, if necessary with the help of a professional advisor.",
+                                       br(),br(),
+                                       h4("Sales restrictions and Fund Documents"),
+                                       "The investment funds mentioned on this Site may only be purchased on the basis of the current sales prospectus and the most recent annual report (or monthly, quarterly report, if these are more recent). The key investor information documentation and the most recent reports are all available from",a("this",href="https://www.aegonalapkezelo.hu/",target="_blank"),"website.",
+                                       br(),br(),
+                                       h4("Risk Considerations"),
+                                       "Every investment involves risk, especially with regard to fluctuations in value and return. Investments in foreign currencies involve the additional risk that the foreign currency might lose value against the investor's reference currency. Historical performance indications and financial market scenarios are no guarantee for current or future performance. Performance indications do not consider commissions levied at subscription and/or redemption. Furthermore, no guarantee can be given that the performance of the benchmark will be reached or outperformed. Some investment products include investments in Emerging Markets. Emerging Markets are located in countries that possess one or more of the following characteristics: A certain degree of political instability, relatively unpredictable financial markets and economic growth patterns, a financial market that is still at the development stage or a weak economy. Emerging markets investments usually result in higher risks such as political risks, economical risks, credit risks, exchange rate risks, market liquidity risks, legal risks, settlement risks, market risks, shareholder risk and creditor risk. Equities are subject to market forces and hence fluctuations in value which are not entirely predictable. Investment principal on bonds can be eroded depending on sale price or market price. In addition, there are bonds on which investment principal can be eroded due to changes in redemption amounts. Care is required when investing in such instruments. The key risks of real estate funds include limited liquidity in the real estate market, changing mortgage interest rates, subjective valuation of real estate, inherent risks with respect to the construction of buildings and environmental risks (e.g., land contamination). ",
+                                       br(),br(),
+                                       h4("Licence"),
+                                       a(href="http://creativecommons.org/licenses/by-sa/4.0/",target="_blank",img(style="border-width:0",src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png")),
+                                       " This work is licensed under a",a(href="http://creativecommons.org/licenses/by-sa/4.0/","Creative Commons Attribution-ShareAlike 4.0 International License",target="_blank"),".",
+                                       br()
+                                      )
                         ) # tabBox() end
                        ) # column() end
-              )
-      ),
-
-      tabItem(tabName = "funds_ov",
-              h2("Funds", tags$small("Investment Overview"))
+              ) # fluidRow() end
       ),
 
       tabItem(tabName = "fundprices",
-              h2("Funds", tags$small("Prices")),
+              h2("Funds", tags$small("Details")),
               fluidRow(uiOutput("dyn_ui_block"))
       )
 
