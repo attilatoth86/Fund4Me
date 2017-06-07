d4e81730d8921202385784e5ce66511b742bf2e6
attilatoth86 <attila.toth86@gmail.com>
2017-06-07 15:05:12 +0200

    20_calc_R_dt_dbobj_rep_fund_summary.R:  - adding source object section  - writing into database section

--- 20_calc_R_dt_dbobj_rep_fund_summary.R
+++ 20_calc_R_dt_dbobj_rep_fund_summary.R
@@ -13,6 +13,21 @@ library(plyr)
 library(dplyr)
 source("/srv/shiny-server/fund4me/db_f.R")
 
+message("\n")
+message("Source Object setup..")
+###############################################################################
+sou_obj_desc <- "SO_REP_FUNDSUMEXT_LOADER"
+if(sou_obj_desc!=psqlQuery(sprintf("SELECT constant FROM dw.source_object WHERE constant='%s'",sou_obj_desc))$result[1,1]){
+  stop("Invalid source object!")
+} else {
+  sou_obj_id <- psqlQuery(sprintf("SELECT id FROM dw.source_object WHERE constant='%s';",sou_obj_desc))$result[1,1]
+}
+###############################################################################
+message(paste0("Source Object id: ",sou_obj_id))
+message(paste0("Source Object constant: ",sou_obj_desc))
+
+message("\n")
+
 # init data import --------------------------------------------------------
 
 message("Importing data from rep.fund_summary..")
@@ -134,6 +149,72 @@ saveRDS(dt_dbobj_rep_fund_summary,
 message(list.files(path = "/srv/shiny-server/fund4me/RDS_dt_dbobj_rep_fund_summary/", pattern = ".rds", recursive = F)[1])
 message("\n")
 
+# database operations -----------------------------------------------------
+
+df_to_load <- data.frame(fund_summary_id=dt_dbobj_rep_fund_summary$id,
+                         fund_id=dt_dbobj_rep_fund_summary$fund_id,
+                         source_object_id=sou_obj_id,
+                         date_start=dt_dbobj_rep_fund_summary$date_start,
+                         date_recent=dt_dbobj_rep_fund_summary$date_recent,
+                         date_ytd=dt_dbobj_rep_fund_summary$date_ytd,
+                         date_1m=dt_dbobj_rep_fund_summary$date_1m,
+                         date_3m=dt_dbobj_rep_fund_summary$date_3m,
+                         date_6m=dt_dbobj_rep_fund_summary$date_6m,
+                         date_1yr=dt_dbobj_rep_fund_summary$date_1yr,
+                         date_2yr=dt_dbobj_rep_fund_summary$date_2yr,
+                         date_3yr=dt_dbobj_rep_fund_summary$date_3yr,
+                         date_5yr=dt_dbobj_rep_fund_summary$date_5yr,
+                         date_10yr=dt_dbobj_rep_fund_summary$date_10yr,
+                         price_start=dt_dbobj_rep_fund_summary$price_start,
+                         price_recent=dt_dbobj_rep_fund_summary$price_recent,
+                         price_ytd=dt_dbobj_rep_fund_summary$price_ytd,
+                         price_1m=dt_dbobj_rep_fund_summary$price_1m,
+                         price_3m=dt_dbobj_rep_fund_summary$price_3m,
+                         price_6m=dt_dbobj_rep_fund_summary$price_6m,
+                         price_1yr=dt_dbobj_rep_fund_summary$price_1yr,
+                         price_2yr=dt_dbobj_rep_fund_summary$price_2yr,
+                         price_3yr=dt_dbobj_rep_fund_summary$price_3yr,
+                         price_5yr=dt_dbobj_rep_fund_summary$price_5yr,
+                         price_10yr=dt_dbobj_rep_fund_summary$price_10yr,
+                         nav_recent=dt_dbobj_rep_fund_summary$nav_recent,
+                         name=dt_dbobj_rep_fund_summary$name,
+                         short_name=dt_dbobj_rep_fund_summary$short_name,
+                         isin=dt_dbobj_rep_fund_summary$isin,
+                         asset_manager_name=dt_dbobj_rep_fund_summary$asset_manager_name,
+                         currency=dt_dbobj_rep_fund_summary$currency,
+                         fund_category=dt_dbobj_rep_fund_summary$fund_category,
+                         return_ytd=dt_dbobj_rep_fund_summary$return_ytd,
+                         return_1m=dt_dbobj_rep_fund_summary$return_1m,
+                         return_3m=dt_dbobj_rep_fund_summary$return_3m,
+                         return_6m=dt_dbobj_rep_fund_summary$return_6m,
+                         return_1yr=dt_dbobj_rep_fund_summary$return_1yr,
+                         return_2yr=dt_dbobj_rep_fund_summary$return_2yr,
+                         return_3yr=dt_dbobj_rep_fund_summary$return_3yr,
+                         return_ann_2yr=dt_dbobj_rep_fund_summary$return_ann_2yr,
+                         return_ann_3yr=dt_dbobj_rep_fund_summary$return_ann_3yr,
+                         return_ann_5yr=dt_dbobj_rep_fund_summary$return_ann_5yr,
+                         return_ann_10yr=dt_dbobj_rep_fund_summary$return_ann_10yr,
+                         return_ann_si=dt_dbobj_rep_fund_summary$return_ann_si,
+                         volatility_1yr=dt_dbobj_rep_fund_summary$volatility_1yr,
+                         volatility_2yr=dt_dbobj_rep_fund_summary$volatility_2yr,
+                         volatility_3yr=dt_dbobj_rep_fund_summary$volatility_3yr,
+                         volatility_5yr=dt_dbobj_rep_fund_summary$volatility_5yr,
+                         volatility_10yr=dt_dbobj_rep_fund_summary$volatility_10yr,
+                         drawdown_from=dt_dbobj_rep_fund_summary$drawdown_from,
+                         drawdown_trough=dt_dbobj_rep_fund_summary$drawdown_trough,
+                         drawdown_to=dt_dbobj_rep_fund_summary$drawdown_to,
+                         drawdown_depth=dt_dbobj_rep_fund_summary$drawdown_depth,
+                         drawdown_length=dt_dbobj_rep_fund_summary$drawdown_length,
+                         drawdown_totrough=dt_dbobj_rep_fund_summary$drawdown_totrough,
+                         drawdown_recovery=dt_dbobj_rep_fund_summary$drawdown_recovery)
+
+truncateStatus <- psqlQuery("TRUNCATE TABLE ld.ld_fund_summary_ext;")
+message(paste0("Truncate ld.ld_fund_summary_ext table........",truncateStatus$errorMsg))
+insertLdStatus <- psqlInsert_ld(df_to_load,"ld_fund_summary_ext")
+message(paste0("Insert into ld.ld_fund_summary_ext table........",insertLdStatus$errorMsg))
+insertStatus <- psqlQuery("INSERT INTO rep.fund_summary_ext (fund_summary_id,fund_id,source_object_id,date_start,date_recent,date_ytd,date_1m,date_3m,date_6m,date_1yr,date_2yr,date_3yr,date_5yr,date_10yr,price_start,price_recent,price_ytd,price_1m,price_3m,price_6m,price_1yr,price_2yr,price_3yr,price_5yr,price_10yr,nav_recent,name,short_name,isin,asset_manager_name,currency,fund_category,return_ytd,return_1m,return_3m,return_6m,return_1yr,return_2yr,return_3yr,return_ann_2yr,return_ann_3yr,return_ann_5yr,return_ann_10yr,return_ann_si,volatility_1yr,volatility_2yr,volatility_3yr,volatility_5yr,volatility_10yr,drawdown_from,drawdown_trough,drawdown_to,drawdown_depth,drawdown_length,drawdown_totrough,drawdown_recovery)
+                          SELECT fund_summary_id,fund_id,source_object_id,date_start,date_recent,date_ytd,date_1m,date_3m,date_6m,date_1yr,date_2yr,date_3yr,date_5yr,date_10yr,price_start,price_recent,price_ytd,price_1m,price_3m,price_6m,price_1yr,price_2yr,price_3yr,price_5yr,price_10yr,nav_recent,name,short_name,isin,asset_manager_name,currency,fund_category,return_ytd,return_1m,return_3m,return_6m,return_1yr,return_2yr,return_3yr,return_ann_2yr,return_ann_3yr,return_ann_5yr,return_ann_10yr,return_ann_si,volatility_1yr,volatility_2yr,volatility_3yr,volatility_5yr,volatility_10yr,drawdown_from,drawdown_trough,drawdown_to,drawdown_depth,drawdown_length,drawdown_totrough,drawdown_recovery FROM ld.ld_fund_summary_ext") 
+message(paste0("Insert into rep.fund_summary_ext table........",insertStatus$errorMsg))
 
 message(paste("Job ends: ",Sys.time()))
 message("----------------------------------------")
