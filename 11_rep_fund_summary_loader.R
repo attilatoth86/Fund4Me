08a8e0fb530559a28561d4535cb4bb9b0db0e9f0
attilatoth86 <attila.toth86@gmail.com>
2017-06-11 19:54:16 +0200

    11_rep_fund_summary_loader.R: adding SFTQ scenario

--- 11_rep_fund_summary_loader.R
+++ 11_rep_fund_summary_loader.R
@@ -33,12 +33,14 @@ date_ctrl_tbl <- psqlQuery("SELECT f.id, f.short_name,
                               (dw.get_last_fund_price_date(f.id)-'2 years'::INTERVAL)::DATE cal_date_2yr,
                               (dw.get_last_fund_price_date(f.id)-'3 years'::INTERVAL)::DATE cal_date_3yr,
                               (dw.get_last_fund_price_date(f.id)-'5 years'::INTERVAL)::DATE cal_date_5yr,
-                              (dw.get_last_fund_price_date(f.id)-'10 years'::INTERVAL)::DATE cal_date_10yr
+                              (dw.get_last_fund_price_date(f.id)-'10 years'::INTERVAL)::DATE cal_date_10yr,
+                              '2007-10-09'::date cal_date_sftq_start,
+                              '2009-03-09'::date cal_date_sftq_end
                               FROM dw.fund f")$result
 print(date_ctrl_tbl)
 
 for(i in date_ctrl_tbl$id){
-  for(j in colnames(date_ctrl_tbl[,c("cal_date_ytd","cal_date_1m","cal_date_3m","cal_date_6m","cal_date_1yr","cal_date_2yr","cal_date_3yr","cal_date_5yr","cal_date_10yr")])){
+  for(j in colnames(date_ctrl_tbl[,c("cal_date_ytd","cal_date_1m","cal_date_3m","cal_date_6m","cal_date_1yr","cal_date_2yr","cal_date_3yr","cal_date_5yr","cal_date_10yr","cal_date_sftq_start","cal_date_sftq_end")])){
     dt_output <- psqlQuery(sprintf("SELECT MAX(date) dt_output FROM dw.fund_price fp WHERE fp.fund_id=%i AND fp.date<='%s'",i,date_ctrl_tbl[date_ctrl_tbl$id==i,j]))$result 
     if(is.na(dt_output)==T){inp <- format(date_ctrl_tbl[date_ctrl_tbl$id==i,j],"%Y-%m-%d")} else{inp <- format(dt_output[1,1],"%Y-%m-%d")}
     date_ctrl_tbl[date_ctrl_tbl$id==i,gsub('cal_date','date',j)] <- inp
@@ -48,7 +50,7 @@ message("\n")
 message("Calculated dates..")
 print(date_ctrl_tbl)
 
-db_dump <- date_ctrl_tbl[,c("id", "date_start", "date_recent", "date_ytd","date_1m","date_3m","date_6m","date_1yr","date_2yr","date_3yr","date_5yr","date_10yr")]
+db_dump <- date_ctrl_tbl[,c("id", "date_start", "date_recent", "date_ytd","date_1m","date_3m","date_6m","date_1yr","date_2yr","date_3yr","date_5yr","date_10yr","date_sftq_start","date_sftq_end")]
 
 message("\n")
 truncateStatus_ld <- psqlQuery("TRUNCATE TABLE ld.ld_price_dates;")
@@ -62,7 +64,7 @@ truncateStatus <- psqlQuery("TRUNCATE TABLE rep.fund_summary;")
 message(paste0("Truncate rep.fund_summary table........",truncateStatus$errorMsg))
 
 message("\n")
-queryString <- sprintf("INSERT INTO rep.fund_summary (fund_id, source_object_id, date_start, date_recent, date_ytd, date_1m, date_3m, date_6m, date_1yr, date_2yr, date_3yr, date_5yr, date_10yr, price_start, price_recent, price_ytd, price_1m, price_3m, price_6m, price_1yr, price_2yr, price_3yr, price_5yr, price_10yr, nav_recent)
+queryString <- sprintf("INSERT INTO rep.fund_summary (fund_id, source_object_id, date_start, date_recent, date_ytd, date_1m, date_3m, date_6m, date_1yr, date_2yr, date_3yr, date_5yr, date_10yr, price_start, price_recent, price_ytd, price_1m, price_3m, price_6m, price_1yr, price_2yr, price_3yr, price_5yr, price_10yr, nav_recent, price_sftq_start, price_sftq_end)
                           SELECT 
                             core.fund_id, core.source_object_id, core.date_start, core.date_recent, core.date_ytd, core.date_1m, core.date_3m, core.date_6m, core.date_1yr, core.date_2yr, core.date_3yr, core.date_5yr, core.date_10yr,
                             fpstart.price price_start,
@@ -76,7 +78,9 @@ queryString <- sprintf("INSERT INTO rep.fund_summary (fund_id, source_object_id,
                             fp3y.price price_3yr,
                             fp5y.price price_5yr,
                             fp10y.price price_10yr,
-                            fprecent.net_asset_value nav_recent
+                            fprecent.net_asset_value nav_recent,
+                            fpsftqstart.price price_sftq_start,
+                            fpsftqend.price price_sftq_end
                           FROM
                             (
                             SELECT
@@ -92,7 +96,9 @@ queryString <- sprintf("INSERT INTO rep.fund_summary (fund_id, source_object_id,
                               lpd.date_2yr,
                               lpd.date_3yr,
                               lpd.date_5yr,
-                              lpd.date_10yr
+                              lpd.date_10yr,
+                              lpd.date_sftq_start,
+                              lpd.date_sftq_end
                             FROM
                               dw.fund f
                               LEFT OUTER JOIN ld.ld_price_dates lpd ON f.id=lpd.fund_id
@@ -108,6 +114,8 @@ queryString <- sprintf("INSERT INTO rep.fund_summary (fund_id, source_object_id,
                             LEFT OUTER JOIN dw.fund_price fp3y ON core.fund_id=fp3y.fund_id AND core.date_3yr=fp3y.date
                             LEFT OUTER JOIN dw.fund_price fp5y ON core.fund_id=fp5y.fund_id AND core.date_5yr=fp5y.date
                             LEFT OUTER JOIN dw.fund_price fp10y ON core.fund_id=fp10y.fund_id AND core.date_10yr=fp10y.date
+                            LEFT OUTER JOIN dw.fund_price fpsftqstart ON core.fund_id=fpsftqstart.fund_id AND core.date_sftq_start=fpsftqstart.date
+                            LEFT OUTER JOIN dw.fund_price fpsftqend ON core.fund_id=fpsftqend.fund_id AND core.date_sftq_end=fpsftqend.date
                           ;",sou_obj_desc)
 
 execStatus <- psqlQuery(queryString)
